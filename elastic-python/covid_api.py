1d711f36e0fd996e741f75b5a1675cd0efca0285
hyunmin0317 <choihm9903@naver.com>
2022-01-19 17:09:53 +0900

    elastic-stack

---
+++ elastic-python/covid_api.py
@@ -0,0 +1,35 @@
+import urllib.request
+from xml.etree.ElementTree import fromstring, ElementTree
+from elasticsearch import Elasticsearch, helpers
+
+es = Elasticsearch()
+
+docs = []
+
+url = 'http://openapi.seoul.go.kr:8088/547171685163686f35324270474f6e/xml/TbCorona19CountStatus/1/365/'
+response = urllib.request.urlopen(url)
+xml_str = response.read().decode('utf-8')
+
+tree = ElementTree(fromstring(xml_str))
+root = tree.getroot()
+
+for row in root.iter("row"):
+    date = row.find('S_DT').text
+    today = int(row.find('N_HJ').text)
+    confirmed = int(row.find('T_HJ').text)
+    death = int(row.find('DEATH').text)
+    recover = int(row.find('RECOVER').text)
+    doc = {
+        "_index": "covid19",
+        "_source": {
+            "date": date,
+            "today": today,
+            "confirmed": confirmed,
+            "death": death,
+            "recover": recover
+        }
+    }
+    docs.append(doc)
+
+res = helpers.bulk(es, docs)
+print("END")
