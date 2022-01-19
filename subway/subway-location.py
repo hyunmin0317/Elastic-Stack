84f600e86846b969223c46aca20ed2d546ff236a
hyunmin0317 <choihm9903@naver.com>
2022-01-19 19:34:17 +0900

    elastic-stack

---
+++ subway/subway-location.py
@@ -0,0 +1,48 @@
+import csv
+import urllib.request
+from xml.etree.ElementTree import fromstring, ElementTree
+from elasticsearch import Elasticsearch, helpers
+
+def subway_data():
+    f = open('subway.csv', 'r', encoding='utf-8-sig')
+    rdr = csv.reader(f)
+
+    data = []
+
+    for line in rdr:
+        data.append(line)
+    f.close()
+    return data
+
+if '__main__':
+    es = Elasticsearch()
+    data = subway_data()
+    docs = []
+
+    for row in data:
+        line = row[0]
+        station = row[1]
+        number = row[2]
+        place_x = row[3]
+        place_y = row[4]
+
+        if place_x != '' and place_y != '':
+            place_x = float(place_x)
+            place_y = float(place_y)
+
+            doc = {
+                "_index": "seoul_subway",
+                "_source": {
+                    "line": line,
+                    "station": station,
+                    "number": number,
+                    "instl_xy": {
+                        "lat": place_x,
+                        "lon": place_y
+                    }
+                }
+            }
+            docs.append(doc)
+
+res = helpers.bulk(es, docs)
+print("END")
